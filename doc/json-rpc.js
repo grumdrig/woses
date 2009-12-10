45311c817ed785f3d08dc4b1c866777b8ab9d1db
Eric Fredricksen <efredricksen@gmail.com>
2009-12-09 23:09:50 -0800

    Rename a couple files

--- doc/json-rpc.js
+++ doc/json-rpc.js
@@ -1,8 +1,6 @@
 var sys = require("sys");
 
 exports.fetch = function (request, response) {
-  sys.p(request);
-  sys.p("WTF");
   response.header("content-type", "application/json");
   response.body = {sum: request.json.a + request.json.b,
                    aka: request.params.a + request.params.b};
